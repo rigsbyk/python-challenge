097fd77734ac82a2a572ad44f41fe531fabd43c4
rigsbyk <kathryn.rigsby@gmail.com>
2020-06-29 08:22:52 -0400

    cleaned up code and comments in both main-practice and main

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,55 +8,61 @@ import csv
 data_budget = os.path.join("Resources","budget-data.csv")
 
 
-#initially setting the variables changes, net_total, previous and total_months to 0
+#initially setting the variables changes
 changes = 0
 net_total = 0
 previous = 0
 total_months = 0 
 
 #created lists to store data
+date = [] 
 profits = []
-date = []
 
-# using the data_budget path, open the file; using alias csvfile
-# csvreader iterates over each line of the csvfile using the comma as a field separator(delimiter)
+
+#opens up the csvfile from the data_budget path provided above
+#csvreader iterates over each line of the csvfile
 with open(data_budget) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-
-    # next(csvreader) just reads over the header; removing it from calculations
+    
+    #next(csvreader) just reads over the header; removing it from calculations
     csv_header = next(csvreader)
-
-     #next(csvreader) on the first row so that it can correctly calculate the Average Change (as the first row doesn't have anything to calculate from)
+    
+    #next(csvreader) on the first row so that it can calculate the Average Change
+    #starting with the second row of calcuable data
     first_row = next(csvreader)
     previous = int(first_row[1])
     net_total = net_total + int(first_row[1])
     total_months = total_months + 1
-
-    # going through each row after the header and first_row
+    
+    #going through each row after the header and first_row
     for row in csvreader:
-        # using a count to get total number of rows 
+        
+        #using a count to get total number of rows for total months count
         total_months += 1
-
-        # using a counter to total up Profit/Losses
-        net_total += int(row[1])
-
-        # as it goes through for row in csvreader, it calculates the changes
-        # Then it adds those calculations to the profit list
+        
+        #using a counter to total up Profit/Losses
+        net_total = net_total + int(row[1])
+     
+        #as it goes through for row in csvreader, it calculates the changes
+        #Then it adds those calculations to the profit list
         changes = int(row[1]) - previous
         profits.append(changes)
         previous = int(row[1])
-
+        
+        #calculating greatest increase/decrease of profits using stored profits data
         greatestincrease = max(profits)
         greatestdecrease = min(profits)
         
+        #grabbing the associated date of greatest increase/decrease using the index list method
         date.append(row[0])
         idate = date[profits.index(greatestincrease)]
         ddate = date[profits.index(greatestdecrease)]
+    
+    
+#after calculating above you find the average of those changes to find the Average Change       
+average_change = round(sum(profits)/len(profits),2)
 
-# after calculating above you find the average of those changes to find the Average Change       
-average_change = round(sum(profits) / len(profits), 2)
-
-##created a string to be able to call on for both print and output
+#created a string to be able to call on for both print and output
 results = (
             f"Financial Analysis\n"
             f"---------------------\n"
