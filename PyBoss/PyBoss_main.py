c323bf5a39df260280e77e12250bb35506c23f06
rigsbyk <kathryn.rigsby@gmail.com>
2020-06-30 13:55:46 -0400

    updated PyBoss_main.py comments

--- PyBoss/PyBoss_main.py
+++ PyBoss/PyBoss_main.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
 
-
+# Import os provides function for interacting with the operating system(OS)
+# Import csv is a module for importing and reading csv files
+# Import datetime to change the format of the DOB
 import os
 import csv
 import datetime
 
-
-# In[2]:
-
-
+# Provides the path of the csv file "budget-data.csv"
 emp_data = os.path.join("Resources", "employee_data.csv")
 
 
-# In[3]:
-
-
+# List of states and abbreviations from link provided in assignment
 us_state_abbrev = {
     'Alabama': 'AL',
     'Alaska': 'AK',
@@ -72,9 +68,7 @@ us_state_abbrev = {
 }
 
 
-# In[4]:
-
-
+# Lists to store data
 firstname = []
 lastname = []
 empid = []
@@ -83,86 +77,60 @@ employeessn = []
 employeest = []
 
 
-# In[5]:
-
-
+# Opens up the csvfile from the data_budget path provided above
+# CSVreader iterates over each line of the csvfile
 with open(emp_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     
+    # Next(csvreader) just reads over the header; removing it from calculations/results
     csv_header = next(csvreader)
     
+    # Going through each row after the header and first_row
     for row in csvreader:
         
-        #employee id
+        # Employee id
         empid.append(row[0])
         
-        #split first and last name by using rsplit()
+        # Split first and last name by using rsplit()
         name = row[1]
         fname = name.rsplit(' ',1)[0]
         firstname.append(fname)
         lname = name.rsplit(' ',1)[1]
         lastname.append(lname)
         
-        #stripped the old date format and created new date format
+        # Stripped the old date format and created new date format
         newdob = datetime.datetime.strptime(row[2],"%Y-%m-%d")
         newdob = newdob.strftime("%m/%d/%Y")
         employeedob.append(newdob)
         
-        #masked the first 7 characters of ssn by split and join string methods
+        # Masked the first 7 characters of ssn by split and join string methods
         split_ssn = list(row[3])
         split_ssn[0:3] = ("*","*","*")
         split_ssn[4:6] = ("*","*")
         joined_ssn = "".join(split_ssn)
         employeessn.append(joined_ssn)
         
-        #abbreviated state name
+        # Abbreviated state name
         stateabbrev = us_state_abbrev[row[4]]
         employeest.append(stateabbrev)
 
-
-# In[6]:
-
-
+# Zip lists together
 cleaned_csv = zip(empid,firstname,lastname,employeedob,employeessn,employeest)
 
 
-# In[7]:
-
-
+# Set variable for output file
 output_file = os.path.join("New Format","Reformatted.csv")
 
-
-# In[8]:
-
-
+#  Open the output file
 with open(output_file, "w") as datafile:
     writer = csv.writer(datafile)
     
+    # Write the header row
     writer.writerow(["Emp ID", "First Name", "Last Name", "DOB",
                      "SSN", "State"])
     
+    # Write in zipped rows
     writer.writerows(cleaned_csv)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
